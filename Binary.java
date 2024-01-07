06d145635c071a8cbeb4833486ed1786c30cf82b
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2024-01-07 13:25:01 +0530

    Java
    Program that adds to binary digit and display it.

---
+++ Binary.java
@@ -0,0 +1,13 @@
+class Binary
+{
+    public static void main(String args[])
+    {
+        int num1= 10;
+        int num2=12;
+        System.out.println("Num1 ="+Integer.toBinaryString(num1));
+        System.out.println("Num2 ="+Integer.toBinaryString(num2));
+        
+        int add=num1+num2;
+        System.out.print("Addition ="+Integer.toBinaryString(add));
+    }
+}
