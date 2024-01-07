da99d24f0a489e4323f12c33d0f8d1a8fdd31e98
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2024-01-07 12:37:43 +0530

    Java
    java program to display a pattern structure.

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+class Pattern
+{
+    public static void main(String args[])
+    {
+        for(int i=5;i>0;i--)
+        {
+            for(int j=i;j>0;j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
